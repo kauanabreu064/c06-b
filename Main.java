c414a9c0d445ad021ef6562811691c35641ca010
Kauan abreu <83833447+kauanabreu064@users.noreply.github.com>
2026-02-23 20:38:36 -0300

    primeiro arquivo

---
+++ Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Locale;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            int primeira = 10;
+            int segunda = 4;
+            int terceira = 2;
+            int soma = primeira + segunda + terceira;
+            double media = soma/3.0;
+
+            System.out.println(soma);
+            System.out.println(media);
+        }
+}
